4824508ca2ad718dafd270125176b5763e782972
Aditya Saini <lavanyasaini615@gmail.com>
2025-03-25 03:38:28 +0530

    added js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".contact").addEventListener("click", function () {
+  window.location.href =
+    "https://www.linkedin.com/in/lavanya-saini-556a36324?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+});
